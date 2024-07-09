c3d268832e119049a2f2a21c12ee831be718ce77
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-09 09:37:01 +0000

    task completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -14,8 +14,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
 }
